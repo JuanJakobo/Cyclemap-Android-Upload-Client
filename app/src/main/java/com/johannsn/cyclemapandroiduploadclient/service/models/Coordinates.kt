446cdf69d0208f1e45064c12374cc0eaa56e94fe
JuanJakobo <github@johannsn.com>
2022-09-21 11:16:14 +0200

    Make TripFragment editable

--- app/src/main/java/com/johannsn/cyclemapandroiduploadclient/service/models/Coordinates.kt
+++ app/src/main/java/com/johannsn/cyclemapandroiduploadclient/service/models/Coordinates.kt
@@ -1,7 +1,7 @@
 package com.johannsn.cyclemapandroiduploadclient.service.models
 
 data class Coordinates (
-    val long: Double? = 0.0,
+    val lng: Double? = 0.0,
     val lat: Double? = 0.0,
     val altitude: Double? = 0.0,
     val id: Long = 0,
--- app/src/main/java/com/johannsn/cyclemapandroiduploadclient/service/models/Trip.kt
+++ app/src/main/java/com/johannsn/cyclemapandroiduploadclient/service/models/Trip.kt
@@ -2,6 +2,7 @@ package com.johannsn.cyclemapandroiduploadclient.service.models
 
 data class Trip(
     val id: Long = 0,
+    val title: String,
     val text: String,
     var tour: Tour? = null,
     val coordinates: MutableList<Coordinates>? = mutableListOf(),
