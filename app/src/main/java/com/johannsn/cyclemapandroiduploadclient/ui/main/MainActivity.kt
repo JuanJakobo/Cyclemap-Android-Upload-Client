c0127f166ae3185c0104ecdefbddfbc81466efd6
JuanJakobo <github@johannsn.com>
2022-09-22 11:31:03 +0200

    enable receiving fit files from wahoo

--- app/src/main/java/com/johannsn/cyclemapandroiduploadclient/ui/main/MainActivity.kt
+++ app/src/main/java/com/johannsn/cyclemapandroiduploadclient/ui/main/MainActivity.kt
@@ -1,17 +1,26 @@
 package com.johannsn.cyclemapandroiduploadclient.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.Parcelable
 import android.preference.PreferenceManager
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import com.johannsn.cyclemapandroiduploadclient.R
 import com.johannsn.cyclemapandroiduploadclient.databinding.ActivityMainBinding
+import com.johannsn.cyclemapandroiduploadclient.service.fitToGeo
+import com.johannsn.cyclemapandroiduploadclient.service.models.Tour
+import com.johannsn.cyclemapandroiduploadclient.service.models.Trip
 import org.osmdroid.config.Configuration
+import org.osmdroid.util.GeoPoint
+import java.io.IOException
 import java.util.*
 
 
@@ -22,13 +31,47 @@ class MainActivity : AppCompatActivity() {
 
     private val REQUEST_PERMISSIONS_REQUEST_CODE = 1
 
+    internal var geoPoints = Vector<GeoPoint>()
+    internal var currentTour : Tour? = null
+    internal var currentTrip : Trip? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //TODO move?
         Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this))
 
+        //TODO
+        //https://stackoverflow.com/questions/16499385/using-bundle-to-pass-data-between-fragment-to-another-fragment-example
+        //https://github.com/nextcloud/android
+        //TODO kotlin non null assesment vs assement
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        if(intent  != null) {
+            if (intent.type.equals("message/rfc822")) {
+                val stream : MutableList<Parcelable>? = when (intent.action) {
+                    Intent.ACTION_SEND_MULTIPLE ->
+                        intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM)!!
+                    Intent.ACTION_SEND ->
+                        mutableListOf(intent.getParcelableExtra(Intent.EXTRA_STREAM)!!)
+                    else -> null
+                }
+                if (stream != null) {
+                    val uri = Uri.parse(stream[stream.size-1].toString())
+                    try {
+                        val test = fitToGeo()
+                        val contentResolver = contentResolver
+                        val inputStream = contentResolver?.openInputStream(uri)
+                        if(inputStream != null) {
+                            geoPoints = test.readInFit(inputStream)
+                        }
+                    } catch (e: IOException) {
+                        throw RuntimeException("Error opening file")
+                    }
+                }
+            }
+        }
 
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
